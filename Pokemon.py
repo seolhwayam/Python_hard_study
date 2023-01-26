e8dde60bebd8885eed5fc45b9b5289925388b236
study <susung462@gmail.com>
2023-01-26 23:08:49 +0900

    Pokemon project

---
+++ Pokemon.py
@@ -0,0 +1,83 @@
+class Pokemon:
+    def __init__(self, owner, skills):
+        self.owner = owner
+        self.skills = skills.split('/')
+        print(f'포켓몬 생성 :', end=' ')
+
+    def info(self):
+        print(f'{self.owner}의 포켓몬이 사용 가능한 스킬')
+        n = 1
+        for skill in self.skills:
+            print(str(n) + " : " + skill)
+            n =n+1
+        print("공격 번호 선택 : ")
+
+    def attack(self, idx):
+        print(f'{self.skills[idx]} 공격 시전!')
+
+
+
+class Pikachu(Pokemon):
+    def __init__(self, owner, skills):
+        super().__init__(owner, skills)
+        self.name = "피카츄"
+        print(f'{self.name}')
+
+    def attack(self, idx):
+        print(f'[삐까삐까] {self.owner}의 {self.name}가 {self.skills[idx]} 공격 시전!')
+
+
+class Ggoboogi(Pokemon):
+    def __init__(self, owner, skills):
+        super().__init__(owner, skills)
+        self.name = "꼬부기"
+        print(f'{self.name}')
+
+    def attack(self, idx):
+        print(f'[꼬북꼬북] {self.owner}의 {self.name}가 {self.skills[idx]} 공격 시전!')
+
+class Pairi(Pokemon):
+    def __init__(self, owner, skills):
+        super().__init__(owner, skills)
+        self.name = "파이리"
+        print(f'{self.name}')
+
+    def attack(self, idx):
+        print(f'[파읠파읠] {self.owner}의 {self.name}가 {self.skills[idx]} 공격 시전!')
+
+
+
+
+while True:
+    print("1) 포켓몬 생성  2) 프로그램 종료 : ")
+    choice = int(input())
+    if choice == 1:
+         print("1) 피카츄  2)꼬부기 3)파이리")
+         pick = int(input())
+         print("플레이어 이름 입력 :")
+         name = input()
+         print("사용 가능한 기술 입력(/로 구분하여 입력)")
+         skilll = input()
+         if pick == 1:
+            pika1 = Pikachu(name,skilll)
+            pika1.info()
+            attackpick = int(input())-1
+            pika1.attack(attackpick)
+         elif pick == 2:
+            Ggoboo1 = Ggoboogi(name,skilll)
+            Ggoboo1.info()
+            attackpick = int(input())-1
+            Ggoboo1.attack(attackpick)
+         elif pick == 3:
+            Pairi1 = Pairi(name,skilll)
+            Pairi1.info()
+            attackpick = int(input())-1
+            Pairi1.attack(attackpick)
+    elif choice == 2:
+        print("프로그램을 종료합니다")
+        break
+    else :
+        print("잘못 입력되었습니다. 다시 선택해주세요.")
+
+
+
---
+++ dictionary_02.py
@@ -0,0 +1,79 @@
+#dictonary
+
+# 딕셔너리 키 값 출력
+# 딕셔너리 이름.keys()
+# fruits.keys()
+
+# 딕셔너리 밸류 값 출력
+# 딕셔너리 이름.values()
+# fruits.values()
+
+# 딕셔너리 키,밸류 값 출력
+# 딕셔너리 이름.items()
+# fruits.items()
+
+# 딕셔너리 반복문
+# 기본값으로 키 값을 꺼낸다.
+# for k in fruits:
+#     print(k)
+
+
+fruits = {'apple':'사과',
+          'watermelon':'수박',
+          'strawberry':'딸기',
+          'kiwi':'키위',
+          'banana':'바나나'}
+#키 값만 출력 keys()
+print(fruits.keys())
+
+#밸류 값만 출력 values()
+print(fruits.values())
+
+#키,밸류 세트 출력 items()
+print(fruits.items())
+
+#반복문으로 키, 밸류값 찍기
+for k,v in fruits.items():
+    print(k,'\t',v)
+
+#기본값으로 키 값을 꺼낸다.
+for k in fruits:
+    print(k)
+
+
+#음식 추천 프로그램 v0.1
+# alcohol_foods = {'맥주':'치킨','와인':'치즈','고량주':'짬뽕','소주':'골뱅이소면'}
+# alcohol = input('주문하실 술(맥주/와인/고량주/소주)은 ?')
+#
+# if alcohol in alcohol_foods.keys():  #알코올(입력받은)이 알코올푸드 키값에 해당한다면?
+#     print('{0}에 어울리는 안주는 {1}입니다.'.format(alcohol, alcohol_foods[alcohol]))
+
+
+#음식 추천 프로그램 v0.2
+# alcohol_foods = {'맥주':'치킨','와인':'치즈','고량주':'짬뽕','소주':'골뱅이소면'}
+# while True:
+#     alcohol = input('주문하실 술(맥주/와인/고량주/소주/결재)은 ?')
+#     if alcohol =='결재':
+#         break
+#     if alcohol in alcohol_foods.keys():
+#         print('{0}에 어울리는 안주는 {1}입니다.'.format(alcohol, alcohol_foods[alcohol]))
+#     else:
+#         print('{0}은 판매하지 않습니다. 메뉴에서 골라주세요~'.format(alcohol))
+
+
+#음식 추천 프로그램 v0.3
+import random
+star = ['테란','저그','프로토스']
+print(random.choice(star))
+
+alcohol_foods = {'맥주':'치킨','와인':'치즈','고량주':'짬뽕','소주':'골뱅이소면'}
+while True:
+    alcohol = input('주문하실 술(맥주/와인/고량주/소주/아무거나/결재)은 ?')
+    if alcohol =='결재':
+        break
+    if alcohol in alcohol_foods.keys():
+        print('{0}에 어울리는 안주는 {1}입니다.'.format(alcohol, alcohol_foods[alcohol]))
+    elif alcohol == '아무거나'
+        print(rancom.choice(alcohol_foods))
+    else:
+        print('{0}은 판매하지 않습니다. 메뉴에서 골라주세요~'.format(alcohol))
